786c8d4d6484a30f19d600f678a5de0a3d161610
MH DIP <mahmudul.hasan@upaybd.com>
2022-12-28 16:45:58 +0600

    port updated

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"github.com/mahmudulhasandip/go-bookstore/pkg/routes"
 	"log"
 	"net/http"
-	"os"
 )
 
 //func init() {
@@ -17,5 +16,6 @@ func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
+	//log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
+	log.Fatal(http.ListenAndServe(":"+`${{ MY_VAR }}`, r))
 }
